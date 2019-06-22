b21a5c59f2f6e8eb7b05cb5067f2430b2214217f
howardzchung@gmail.com <howardzchung@gmail.com>
2019-06-22 16:06:00 -0700

    add some todos

--- src/components/Heroes/Heroes.tsx
+++ src/components/Heroes/Heroes.tsx
@@ -19,6 +19,11 @@ export class Heroes extends React.Component {
 
 const GetHeroImage = ( heroName: string ) => `https://api.opendota.com/apps/dota2/images/heroes/${heroName}_full.png?`;
 
+// TODO get the hero names from https://github.com/SteamDatabase/GameTracking-Underlords/blob/master/game/dac/panorama/localization/dac_english.txt
+
+// TODO temporary alliance icons until we have actual assets:
+// https://attackofthefanboy.com/guides/dota-underlords-how-to-use-alliances/
+// https://cdn.attackofthefanboy.com/wp-content/uploads/2019/06/assassin-alliance-logo.jpg
 const HeroCard = ( props: { heroName: string } ) => {
     const hero = heroes[props.heroName as keyof typeof heroes];
     return <div className={styles.HeroCard}>
@@ -32,10 +37,12 @@ const HeroCard = ( props: { heroName: string } ) => {
         <div className={styles.Subtitle}>Armor: {hero.armor}</div>
         <div className={styles.Subtitle}>Attack Range: {hero.attackRange}</div>
         <div className={styles.Subtitle}>Attack Rate: {hero.attackRate}</div>
-        <div className={styles.Subtitle}>Health: {hero.health}</div>
+        { /* TODO get the top HP/damage value and show bar relative to max */ }
+        <div className={styles.Subtitle}>Health: {hero.health instanceof Array ? hero.health.join( ' / ') : hero.health }</div>
         {hero.damageMin && <div>Damage Min: {hero.damageMin instanceof Array ? hero.damageMin.join(' / ') : hero.damageMin}</div>}
         {hero.damageMax && <div>Damage Max: {hero.damageMax instanceof Array ? hero.damageMax.join(' / ') : hero.damageMax}</div>}
+        <div className={styles.Subtitle}>Alliances: {hero.keywords}</div>
+        { hero.keywords ? hero.keywords.split(' ').map( keyword => <img className={styles.AllianceIcon} src={`https://cdn.attackofthefanboy.com/wp-content/uploads/2019/06/${keyword}-alliance-icon-dota-underlords.jpg`} />) : null}
         <div>{hero.abilities}</div>
-
     </div>
 }
