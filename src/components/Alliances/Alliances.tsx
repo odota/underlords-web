6573106daeae3c4c0ac97df4bbdc9b506ebdd80f
Albert Cui <aqc2109@columbia.edu>
2019-06-25 01:42:51 +0000

    styling for alliances

--- src/components/Alliances/Alliances.tsx
+++ src/components/Alliances/Alliances.tsx
@@ -3,36 +3,84 @@ import alliances from 'dotaconstants/build/underlords_alliances.json';
 import heroes from 'dotaconstants/build/underlords_heroes.json';
 import styles from './Alliances.module.css';
 import commonStyles from '../common.module.css';
+import gameStrings from 'dotaconstants/build/underlords_localization_en.json';
+
+// // TODO: Is there a better way to define this?
+type Hero = typeof heroes.abaddon;
+type Alliance = typeof alliances[keyof typeof alliances] & { 
+  name: string
+  heroes: Hero[]
+};
+type GameStrings = { [key: string]: string };
 
 export default class Alliances extends React.Component {
 
   state = {
-    alliances: {},
-    heroes: {},
-    loading: true,
-    error: false
+    alliances: [],
+    heroes: []
   };
+
+  // TODO: Is there a better way to avoid this error?
+  // ts(7053): "Element implicitly has an 'any' type because expression of type 'string' can't be used to index type"
+  gameStrings: { [key: string]: string } = gameStrings;
 
   componentDidMount() {
     this.setState({
-      alliances: alliances,
-      heroes: heroes
+      alliances: Object.keys(alliances).map((key) => {
+        let a: Partial<Alliance> = alliances[key as keyof typeof alliances];
+        a.name = key.toLowerCase();
+        
+        let allianceHeroes: Hero[] = [];
+        Object.entries(heroes).forEach(([k, v]) => {
+          if (v.draftTier > 0 && v.keywords &&  v.keywords.includes(a.name as string)) {
+            if (v.displayName) {
+              v.displayName = this.gameStrings[v.displayName.replace("#", "")];
+            }
+            allianceHeroes.push(v as Hero);
+          }
+        })
+        a.heroes = allianceHeroes;
+        return a;
+      }) as Alliance[],
     })
+
   }
 
   render() {
-    if ( this.state.error )
-    {
-      return <div>Error</div> ;
-    }
-    return <div>
-          {Object.entries(this.state.alliances).map(([key, val]) => {
-            return <div className={styles.AllianceCard}>
-              <img src={`${process.env.PUBLIC_URL}/images/alliances/${key}.jpg`} />
-              <div className={commonStyles.Title}>{key}</div>
-              <div>{JSON.stringify(val)}</div>
-            </div>
+    return <div className={commonStyles.CardsContainer}>
+          {this.state.alliances.map((alliance: Alliance, i: number) => {
+            const maxUnits = alliance.levels[alliance.levels.length - 1].unitcount;
+
+            return <div className={commonStyles.Card} key={i}>
+                <div className={commonStyles.CardCapSmallImage}>
+                  <img src={`${process.env.PUBLIC_URL}/images/alliances/${alliance.name}.jpg`} />
+                  <div className={commonStyles.Title}>{this.gameStrings[`dac_synergy_${alliance.name}`]}</div>
+                </div>
+                <div>{(alliance.levels as any[]).map((e: any, i: number) => {
+                  return <div>
+                      <UnitIndicator num={e.unitcount} max={maxUnits} color={alliance.color}/>
+                      <p className={styles.Effect}>{this.gameStrings[`dac_synergy_desc_${alliance.name}_${i + 1}`]}</p>
+                    </div>
+                })}</div>
+                <div>{JSON.stringify(alliance)}</div>
+              </div>
           })}
-      </div>
+        </div>
   }
+}
+
+const UnitIndicator = (props: { num: number, max: number, color: string}) => {
+  const { num, max, color } = props;
+  return <div className={styles.UnitCountContainer}>
+    {Array.from(Array(max).keys()).map((e: any, i: number) => {
+      let style = {
+        backgroundColor: 'initial'
+      }
+      if (i < num) {
+        style.backgroundColor = `rgb(${color})`;
+      }
+      return <div className={styles.Unit} style={style}/>
+    })}
+  </div>
+
 }
--- src/components/Heroes/Heroes.tsx
+++ src/components/Heroes/Heroes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import heroes from 'dotaconstants/build/underlords_heroes.json';
 import abilities from 'dotaconstants/build/underlords_abilities.json';
 import gameStrings from 'dotaconstants/build/underlords_localization_en.json';
+import abilityStrings from 'dotaconstants/build/underlords_localization_abilities_en.json'
 import styles from './Heroes.module.css';
 import commonStyles from '../common.module.css';
 
@@ -9,13 +10,15 @@ import commonStyles from '../common.module.css';
 // howardc: Not sure why the typing fails if we use keyof typeof heroes, maybe too many different unions confuses TS?
 type Hero = typeof heroes.abaddon;
 type Ability = typeof abilities[keyof typeof abilities];
+type Abilities = { [key: string]: Ability };
 type GameStrings = { [key: string]: string };
+const ABILITY_REGEX = /({s:[^}]*})/g;
 
 export default class Heroes extends React.Component {
     state = {
         heroes: [],
         npc: [],
-        sort: "displayName",
+        sort: 'displayName',
         isAscending: false //so that it's ascending on componentMount
     }
 
@@ -24,7 +27,9 @@ export default class Heroes extends React.Component {
         let npc: Hero[] = [];
         Object.entries(heroes).forEach(([k, v]) => {
             const strings: GameStrings = gameStrings;
-            v.displayName = strings[v.displayName.replace("#", "")];
+            if (v.displayName) {
+                v.displayName = strings[v.displayName.replace('#', '')];
+            }
             if (v.draftTier > 0) {
                 realHeroes.push(v as Hero);
             } else {
@@ -36,7 +41,7 @@ export default class Heroes extends React.Component {
             heroes: realHeroes,
             npc: npc,
         }, () => {
-            this.sortHeroes("displayName");
+            this.sortHeroes('displayName');
         });
     }
 
@@ -61,54 +66,77 @@ export default class Heroes extends React.Component {
     public render() {
         return <div>
             <div>
-                <button onClick={(e) => this.sortHeroes("draftTier")}>Order by Tier/Cost</button>
-                <button onClick={(e) => this.sortHeroes("displayName")}>Order by Name</button>
+                <button onClick={(e) => this.sortHeroes('draftTier')}>Order by Tier/Cost</button>
+                <button onClick={(e) => this.sortHeroes('displayName')}>Order by Name</button>
             </div>
             <pre>{JSON.stringify( heroes['abaddon'], null, 2 )}</pre>
-            <div style={{ display: 'flex', flexWrap: 'wrap', height: '100vh', overflow: 'scroll' }}>
+            <div className={commonStyles.CardsContainer}>
                 { this.state.heroes.map( e => <HeroCard hero={e} ></HeroCard>)}
             </div>
         </div>
     }
 }
 
-const GetHeroImage = ( dotaName: string ) => `https://api.opendota.com/apps/dota2/images/heroes/${dotaName.replace("npc_dota_hero_", "")}_full.png?`;
+const GetHeroImage = ( dotaName: string ) => `https://api.opendota.com/apps/dota2/images/heroes/${dotaName.replace('npc_dota_hero_', '')}_full.png?`;
 
 // TODO get the hero names from https://github.com/SteamDatabase/GameTracking-Underlords/blob/master/game/dac/panorama/localization/dac_english.txt
 // TODO ability descriptions are in https://github.com/SteamDatabase/GameTracking-Underlords/blob/master/game/dac/pak01_dir/resource/localization/dac_abilities_english.txt
 const HeroCard = ( props: { hero: Hero } ) => {
     const hero = props.hero;
-    const ability = abilities[hero.abilities[0] as keyof typeof abilities];
-    return <div className={styles.HeroCard}>
-            <div className={styles.CardCap}>
+    // const ability = abilities[hero.abilities[0] as keyof typeof abilities];
+    return <div className={commonStyles.Card}>
+            <div className={commonStyles.CardCap}>
                 <img className={styles.HeroImage} src={GetHeroImage(hero.dota_unit_name)} />
-                <div className={styles.CardCapContent}>
-                    <div className={styles.CardCapContentContainer}>
+                <div className={commonStyles.CardCapContent}>
+                    <div className={commonStyles.CardCapContentContainer}>
                         <div className={commonStyles.Title}>{hero.displayName}</div>
                         <div className={commonStyles.Midtitle}>Tier/Cost {hero.draftTier}</div>
                     </div>
                 </div>
             </div>
-            <div className={styles.CardBody}>
+            <div className={commonStyles.CardBody}>
                 <div className={commonStyles.Subtitle}>Armor: {hero.armor}</div>
                 <div className={commonStyles.Subtitle}>Attack Range: {hero.attackRange}</div>
                 <div className={commonStyles.Subtitle}>Attack Rate: {hero.attackRate}</div>
                 { /* TODO get the top HP/damage value and show bar relative to max */ }
                 <div className={commonStyles.Subtitle}>Health: {hero.health instanceof Array ? hero.health.join( ' / ') : hero.health }</div>
-                {hero.damageMin && <div>Damage Min: {hero.damageMin instanceof Array ? hero.damageMin.join(' / ') : hero.damageMin}</div>}
-                {hero.damageMax && <div>Damage Max: {hero.damageMax instanceof Array ? hero.damageMax.join(' / ') : hero.damageMax}</div>}
+                {hero.damageMin && <div className={commonStyles.Subtitle}>Damage Min: {hero.damageMin instanceof Array ? hero.damageMin.join(' / ') : hero.damageMin}</div>}
+                {hero.damageMax && <div className={commonStyles.Subtitle}>Damage Max: {hero.damageMax instanceof Array ? hero.damageMax.join(' / ') : hero.damageMax}</div>}
                 <div className={commonStyles.Subtitle}>Alliances: {hero.keywords}</div>
                 { hero.keywords ? 
                     hero.keywords.split(' ').map( (keyword: string) => {
                         return <img className={styles.AllianceIcon} alt={keyword} src={`${process.env.PUBLIC_URL}/images/alliances/${keyword}.jpg`} />;
                     }) 
                     : null}
-                { hero.abilities.map( abilityKey => <AbilityCard abilityKey={abilityKey} /> )}
+                { hero.abilities.map( abilityKey => <AbilityCard abilityKey={abilityKey}/> )}
             </div>
     </div>
 }
 
-const AbilityCard = ( props: { abilityKey: string } ) => {
+const GetAbilityDescription = (abilityKey: string): string => {
+    let desc: string = (abilityStrings.tokens as GameStrings)[`dac_ability_${abilityKey}_Description`];
+    let ability: Ability = (abilities as Abilities)[abilityKey];
+    if (desc) {
+        const matches = desc.match(ABILITY_REGEX);
+        if (matches) {
+            matches.forEach((s: string) => {
+                let replace = '';
+                const key = s.replace('{s:', '').replace('}', '') as keyof Ability;
+                if (key in ability) {
+                    const val: any = ability[key];
+                    replace = Array.isArray(val) ? `[${val.join('/')}]` : val;
+                }
+
+                desc = desc.replace(s, replace);
+            })
+        }
+        return desc.replace(/<br>/g, '\n');
+    } else {
+        return '';
+    }
+
+}
+const AbilityCard = ( props: { abilityKey: string} ) => {
     const { abilityKey } = props;
     return <div className={styles.AbilityCard}>
         { /* <div>{hero.abilities}</div> */ }
@@ -118,6 +146,6 @@ const AbilityCard = ( props: { abilityKey: string } ) => {
             </div>
             <div>{(gameStrings as GameStrings)[`dac_ability_${abilityKey}`] || abilityKey}</div>
         </div>
-        <p>Description here.</p>
+        <p>{GetAbilityDescription(abilityKey)}</p>
     </div>;
 }
