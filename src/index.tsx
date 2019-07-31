683fe70150c03315e828e0ddc87107a3eb67d32c
Albert Cui <aqc2109@columbia.edu>
2019-07-31 11:32:23 -0700

    try adding update logic

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ import { createBrowserHistory } from 'history';
 import ReactGA from 'react-ga';
 import { SUPPORTED_LANGUAGES } from './utils';
 import { getLanguage } from './components/Localization/Localization';
+import { ServiceWorkerProvider } from './context/ServiceWorkerContext';
 
 ReactGA.initialize('UA-55757642-4');
 ReactGA.pageview(window.location.pathname + window.location.search);
@@ -23,16 +24,13 @@ history.listen((location) => {
 ReactDOM.render(
     // @ts-ignore
     <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme, muiTheme)}>
+      <ServiceWorkerProvider>
         <Router>
-            <Switch>
-                <Route path={`/:lang(${Object.values(SUPPORTED_LANGUAGES).join("|")})`} component={App}/>
-                <Redirect to={`/${getLanguage()}/`}/>
-            </Switch>
+          <Switch>
+              <Route path={`/:lang(${Object.values(SUPPORTED_LANGUAGES).join("|")})`} component={App}/>
+              <Redirect to={`/${getLanguage()}/`}/>
+          </Switch>
         </Router>
+      </ServiceWorkerProvider>
     </MuiThemeProvider>    
 , document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
